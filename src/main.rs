4c312eaadb1e651872d6200e15573a94c54176df
tkmktkhr <mukku.wordpress@gmail.com>
2023-07-22 11:21:44 +0900

    install axum & sample api

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use axum::{
+    // http::StatusCode,
+    // response::IntoResponse,
+    routing::{get, post},
+    Json,
+    Router,
+};
 use dotenv::dotenv;
+use serde_json::{json, Value};
 use std::env;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().ok();
     println!("{:?}", env::var("ADDRESS").unwrap_or("".to_string()));
     println!("Hello, world!");
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/json", get(|| async { Json(json!({ "data": 42 })) }));
+
+    let address = env::var("ADDRESS").unwrap_or("".to_string());
+    axum::Server::bind(&address.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap_or_else(|_| panic!("cannot serve"));
 }
